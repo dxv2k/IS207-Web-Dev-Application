114f746fb22b7fdd98d89f1bd5ccb4656092938e
knightV <9569076+dxv2k@users.noreply.github.com>
2021-04-17 00:27:27 +0700

    API working properly, fix all mistakes

--- swag_shop/server.js
+++ swag_shop/server.js
@@ -1,6 +1,6 @@
 var express = require('express'); 
 var app = express(); 
-var bodyParser = require('body-parser'); 
+// var bodyParser = require('body-parser'); 
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/swag-shop');
 
@@ -9,21 +9,19 @@ var WishList = require('./model/wishlist');
 
 app.use(express.json()); 
 app.use(express.urlencoded({extended:false})); 
-// app.use(express.json()); 
-// app.use(express.urlencoded({extended:false})); 
 
-
-app.listen(3000,function(){ 
-    console.log("SWAG Shop API running on port 3000....");
-});  
-
+// Send message for default URL
+// app.get('/', (req, res) => res.send('Hello World with Express'));
+app.get('/', function(req, res){
+    res.send('Hello World!'); 
+}); 
 
 
 app.post('/product',function(request,response){ 
     // var product = new Proudct({
     //     title: requbodyParserest.body.titleprice
     // }); 
-    var product = new Proudct()
+    var product = new Product(); 
     // Below this in
     product.title = request.body.title; 
     product.price = request.body.price; 
@@ -37,4 +35,9 @@ app.post('/product',function(request,response){
         }
     }); 
 }); 
+
+
+app.listen(3000,function(){ 
+    console.log("SWAG Shop API running on port 3000....");
+});  
 
